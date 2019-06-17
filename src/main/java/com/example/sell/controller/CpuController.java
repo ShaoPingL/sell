92c99af2620960f775fce2b95809938b211acddb
ShaoPing <258lsp@sina.com>
2019-06-17 18:28:22 +0800

    死锁测试

--- src/main/java/com/example/sell/controller/CpuController.java
+++ src/main/java/com/example/sell/controller/CpuController.java
@@ -24,4 +24,36 @@ public class CpuController {
         }
     }
 
+    /**
+     * 死锁
+     */
+    Object lock1 = new Object();
+    Object lock2 = new Object();
+    @GetMapping("/deadlock")
+    public void deadlock(){
+        new Thread(()->{
+            synchronized (lock1){
+                try{
+                    Thread.sleep(1000);
+                }catch (Exception e){}
+                synchronized (lock2){
+                    System.out.println("thread1 over");
+                }
+            }
+        }).start();
+
+        new Thread(()->{
+            synchronized (lock2){
+                try{
+                    Thread.sleep(1000);
+                }catch (Exception e){}
+                synchronized (lock1){
+                    System.out.println("thread2 over");
+                }
+            }
+        }).start();
+    }
+
+
+
 }
